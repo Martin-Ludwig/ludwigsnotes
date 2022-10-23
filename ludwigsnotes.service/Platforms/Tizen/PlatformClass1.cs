ebca3f551dd2a14efd8dc866fbd64a39e4858a64
Martin Ludwig <ML.Commits@gmail.com>
2022-10-23 22:27:41 +0200

    init

---
+++ ludwigsnotes.service/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ludwigsnotes.service
+{
+    // All the code in this file is only included on Tizen.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ ludwigsnotes.service/Platforms/iOS/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace ludwigsnotes.service
+{
+    // All the code in this file is only included on iOS.
+    public class PlatformClass1
+    {
+    }
+}
