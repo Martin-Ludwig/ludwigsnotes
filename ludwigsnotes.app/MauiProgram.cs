cb0dd80bd7a8038389b80e094601984a8f60ff04
Martin Ludwig <ML.Commits@gmail.com>
2022-11-06 01:36:00 +0100

    testing

--- ludwigsnotes.app/MauiProgram.cs
+++ ludwigsnotes.app/MauiProgram.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components.WebView.Maui;
-using ludwigsnotes.Data;
 
 namespace ludwigsnotes;
 
@@ -20,7 +19,6 @@ public static class MauiProgram
 			builder.Services.AddBlazorWebViewDeveloperTools();
 		#endif
 		
-		builder.Services.AddSingleton<WeatherForecastService>();
 
 		return builder.Build();
 	}
